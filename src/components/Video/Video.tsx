21e0e7166174a16a383b06cf57c58b320ec516ae
Ekaterina <ekaterinaborodavka3@gmail.com>
2025-10-21 23:34:43 +0300

    added video

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -1,27 +1,18 @@
 import beggin from "../../assets/begin.mp4";
 import inter from "../../assets/inter.mp4";
 import suoermodel from "../../assets/supermodel.mp4";
+import raining_man from "../../assets/raining_man.mp4";
 
-import "./video.css"
+import "./video.css";
+
+const videos = [beggin, inter, suoermodel, raining_man];
 
 const Video: React.FC = () => {
   return (
     <div className="video_container">
-      <video
-        src={beggin}
-        controls
-        className="video_player"
-      />
-      <video
-        src={inter}
-        controls
-        className="video_player"
-      />
-      <video
-        src={suoermodel}
-        controls
-        className="video_player"
-      />
+      {videos.map((v, idx) => (
+        <video key={idx} src={v} controls className="video_player" />
+      ))}
     </div>
   );
 };
